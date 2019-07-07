b4eeb3db0b2acc3895b9cc1203b7ce230248ad36
f2k1de <hi@f2k1.de>
2019-07-07 12:04:06 +0200

    consistent indentation

--- engine.php
+++ engine.php
@@ -1,25 +1,13 @@
 <?php
-
 //клиентское приложение
-
 	$apikey = '';	
-
 	include('telegramStorage.php'); //Подключаем библиотеку
-	
 	$telegram = new TelegramStorage($apikey);
-
 	$localpath = "~/img.png";
-
 	$answer = $telegram->getFile($localpath);
-
-	if($answer){
-
+	if($answer) {
 		echo $answer;
-
-	}else{
-
-        echo $telegram->saveFile($localpath);
-
+	} else {
+		echo $telegram->saveFile($localpath);
 	}
-
 ?>
